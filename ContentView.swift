656645fb3077afb15ed3929db0119b6c382a22ea
Rolando <cah513355@d204.lths.net>
2025-04-08 12:21:18 -0500

    test6

--- ContentView.swift
+++ ContentView.swift
@@ -20,10 +20,8 @@ struct ContentView: View {
         // Christian
         Button("Subtract"){
             answer = number1 - number2
-            
-            //             answer = number1 - number2
-  
         }
+        
         .padding()
         .background(Color(red: 0, green: 0.56, blue: 0))
         .clipShape(Capsule())
