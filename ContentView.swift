2d548dfff7fa525ff4781b8412c8d0a6360fb58f
Elise Rahman <rah513171@d204.lths.net>
2025-04-08 11:43:15 -0500

    added an @ state var for num2

--- ContentView.swift
+++ ContentView.swift
@@ -7,6 +7,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            
+            @State var number2 = 0
         }
     }
 }
