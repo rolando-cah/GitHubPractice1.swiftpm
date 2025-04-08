ebc0ba6011e413e759f100e7716ee997f16e0410
Rolando <cah513355@d204.lths.net>
2025-04-08 11:39:20 -0500

    test2

--- ContentView.swift
+++ ContentView.swift
@@ -7,11 +7,14 @@ struct ContentView: View {
     @State var number1 = 0
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        
+        
+            Text("\(number1)")
+        
+        
+        
+        
+        
+        
     }
 }
